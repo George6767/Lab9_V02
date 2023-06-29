4e6cf1f7099ae32c42ac2061c597146657d92fbd
George6767 <129250696+George6767@users.noreply.github.com>
2023-06-29 12:47:42 +0300

    47:33 2!+

---
+++ Lab9_V02.Business/Infastructure/ManagersFactory.cs
@@ -0,0 +1,33 @@
+﻿using Lab9_V02.Business.Managers;
+using Lab9_V02.Domain.Interfaces;
+using Lab9_V02.TestData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9_V02.Business.Infastructure
+{
+    public class ManagersFactory
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly StudentManager studentManager;
+        private readonly GroupManager groupManager;
+
+        public ManagersFactory()
+        {
+            unitOfWork = new TestUnitOfWork();
+        }
+        public StudentManager GetSudentManager()
+        {
+            return studentManager
+            ?? new StudentManager(unitOfWork);
+        }
+        public GroupManager GetGroupManager()
+        {
+            return groupManager
+            ?? new GroupManager(unitOfWork);
+        }
+    }
+}
