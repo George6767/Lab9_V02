a357ff65f323d5869cac58e2a00dec465e1ff8f8
George6767 <129250696+George6767@users.noreply.github.com>
2023-06-30 21:09:42 +0300

    1.12.08 ok

---
+++ Lab9_V02/Commands/RelayCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Lab9_V02.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        /// <summary>
+        /// Признак CanExecute
+        /// </summary>
+        private Func<object, bool> canExecute;
+        /// <summary>
+        /// делегат метода Execute
+        /// </summary>
+        private Action<object> executeMethod;
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="executeMethod">делегат метода Execute</param>
+        /// <param name="canExecute">делегат признака Can</param>
+        public RelayCommand(Action<object> executeMethod,Func<object, bool> canExecute)
+        {
+            this.executeMethod = executeMethod;this.canExecute = canExecute;
+        }
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="executeMethod">делегат метода Execute</param>
+        public RelayCommand(Action<object> executeMethod)
+        {
+            this.executeMethod = executeMethod;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            executeMethod(parameter);
+        }
+    }
+}
